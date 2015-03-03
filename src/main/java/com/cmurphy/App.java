b6747b4be844ff4fe55b8c1534a8eef97d2deef9
Christian Murphy <Christian.Murphy@asu.edu>
2015-03-03 13:18:42 -0700

    example code compiles

--- src/main/java/com/cmurphy/App.java
+++ src/main/java/com/cmurphy/App.java
@@ -1,11 +1,57 @@
 package com.cmurphy;
 
-/**
- * Hello world!
- *
- */
+import info.aduna.iteration.Iterations;
+
+import org.openrdf.model.impl.LinkedHashModel;
+import org.openrdf.model.Model;
+import org.openrdf.model.Namespace;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.vocabulary.FOAF;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
+import org.openrdf.model.vocabulary.XMLSchema;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.Rio;
+import org.openrdf.sail.memory.MemoryStore;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws RepositoryException, RDFHandlerException {
+        Repository repository = new SailRepository(new MemoryStore());
+        repository.initialize();
+
+        ValueFactory factory = repository.getValueFactory();
+        String namespace = "http://localhost/";
+
+        URI john = factory.createURI(namespace, "john");
+
+        RepositoryConnection connection = repository.getConnection();
+
+        try {
+            connection.add(john, RDF.TYPE, FOAF.PERSON);
+            connection.add(john, RDFS.LABEL, factory.createLiteral("John", XMLSchema.STRING));
+
+            RepositoryResult<Statement> statements = connection.getStatements(null, null, null, true);
+
+            Model model = Iterations.addAll(statements, new LinkedHashModel());
+            model.setNamespace("rdf", RDF.NAMESPACE);
+            model.setNamespace("rdfs", RDFS.NAMESPACE);
+            model.setNamespace("xsd", XMLSchema.NAMESPACE);
+            model.setNamespace("foaf", FOAF.NAMESPACE);
+            model.setNamespace("ex", namespace);
+
+            Rio.write(model, System.out, RDFFormat.TURTLE);
+        }
+        finally {
+            connection.close();
+        }
     }
+
 }
